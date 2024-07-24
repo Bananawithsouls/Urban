3196b7284406ecaa3091d33335e161106ff705b9
Bananawithsouls <106369332+Bananawithsouls@users.noreply.github.com>
2024-07-24 16:18:35 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+# Ввод трех целых чисел
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+# Условия для проверки количества одинаковых чисел
+if first == second == third:
+    print(3)  # Все числа равны
+elif first == second or first == third or second == third:
+    print(2)  # Хотя бы два числа равны
+else:
+    print(0)  # Все числа разные
