c6d8110934179f08785d2bd3f72b4206380dfd81
Bananawithsouls <106369332+Bananawithsouls@users.noreply.github.com>
2024-10-05 20:55:21 +0400

    Add files via upload

---
+++ multithreading/module_10_3.py
@@ -0,0 +1,52 @@
+import threading
+import random
+import time
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:  # Блокируем доступ к балансу
+                self.balance += amount
+                print(f'Пополнение: {amount}. Баланс: {self.balance}')
+                # Проверяем, если баланс >= 500 и замок заблокирован
+                if self.balance >= 500 and not self.lock.locked():
+                    self.lock.release()  # Разблокируем, если баланс >= 500
+            time.sleep(0.001)  # Задержка в 0.001 секунды
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            print(f'Запрос на {amount}')
+            with self.lock:  # Блокируем доступ к балансу
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f'Снятие: {amount}. Баланс: {self.balance}')
+                else:
+                    print('Запрос отклонён, недостаточно средств')
+                    self.lock.acquire()  # Блокируем поток, если недостаточно средств
+            time.sleep(0.001)  # Задержка в 0.001 секунды
+
+
+# Создаем объект класса Bank
+bk = Bank()
+
+# Создаем потоки для методов deposit и take
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+# Запускаем потоки
+th1.start()
+th2.start()
+
+# Ждем завершения потоков
+th1.join()
+th2.join()
+
+# Выводим итоговый баланс
+print(f'Итоговый баланс: {bk.balance}')
