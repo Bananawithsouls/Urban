6b32ddb1b4d86d8a3209cb070ed7e59f1a4b59bb
Bananawithsouls <106369332+Bananawithsouls@users.noreply.github.com>
2025-01-01 17:32:21 +0300

    Add files via upload

---
+++ djangogame/myproject/task3/urls.py
@@ -0,0 +1,10 @@
+# task3/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
+]
